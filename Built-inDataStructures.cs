7be149acc2d1a5c75ebb8d2facc794641e542625
shakil-solaiman <solaimanshakil56@gmail.com>
2026-06-12 00:23:20 +0600

    Build-in Data Structure (Dictionary)

--- Built-inDataStructures.cs
+++ Built-inDataStructures.cs
@@ -37,7 +37,6 @@ internal class DataStructureProgram
 
         List<int> listNumber = new List<int> {5,10,15,20};
 
-
         foreach(var print in listNumber)
         {
             Console.WriteLine(print);
@@ -48,10 +47,75 @@ internal class DataStructureProgram
 
         var listing = new List<int> {30,40,80,160};
 
+        listing.Add(4);
+        listing.Add(9);
+        listing.Add(15);
+
+        listing.Remove(9);
+
+
         foreach(var print in listing)
         {
             Console.WriteLine(print);
         }
+
+        
+        //listing.Contains(3); // true
+
+        if (listing.Contains(3))
+        {
+            Console.WriteLine("\nSuccess! The number 3 was found in the data structure.");
+        }
+        else
+        {
+            Console.WriteLine("\nError: The number 3 is missing.");
+        }
+
+
+        //Dictionary 01
+
+        Console.WriteLine("\n\nValues of Dictionary 01:");
+
+        var library = new Dictionary<string,int>();
+
+        library["Apple"] = 5;
+        library["Lichi"] = 10;
+        library["Mango"] = 15;
+
+        foreach(var lib in library)
+        {
+            Console.WriteLine(lib);
+        }
+
+
+        // Use TryGetValue inside an IF statement
+        if (library.TryGetValue("Apple", out int val))
+        {
+            Console.WriteLine($"\n\nFound it! The value of apple is: {val}\n\n");
+        }
+        else
+        {
+            Console.WriteLine("Key not found in the dictionary.\n\n");
+        }
+
+
+        //Dictionary 02
+
+        Console.WriteLine("\nValues of Dictionary 02:");
+
+
+        var dic = new Dictionary<String,int>
+        {
+            {"Coconut", 150},
+            {"Jackfruit", 200},
+            {"Mango", 80}
+        };
+
+        foreach(var print in dic)
+        {
+            Console.WriteLine(print);
+        }
+
 
 
 
