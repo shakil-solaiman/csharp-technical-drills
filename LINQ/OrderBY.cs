eddc76fb5bc770dde085bc43ca8bb61068eb4ae0
shakil-solaiman <solaimanshakil56@gmail.com>
2026-06-13 08:29:55 +0600

    LINQ OrderBy and OrderByDescending exercises for Array and object.

--- LINQ/OrderBY.cs
+++ LINQ/OrderBY.cs
@@ -14,6 +14,8 @@ public class OrderByProgram
 {
     public static void Main()
     {
+        // Problem 01
+
         int[] numbers = {5,1,9,2,3,20,16,15,35,24};
 
         var sortedNumbers = numbers.OrderBy(num => num);
@@ -24,6 +26,20 @@ public class OrderByProgram
         {
             Console.Write($"{items} ");
         }
+
+        // Problem 02
+
+        var sortedNumbersDescending = numbers.OrderByDescending(num => num);
+
+        Console.WriteLine("\n\nSorted By Descending Order: ");
+
+        foreach(var items in sortedNumbersDescending)
+        {
+            Console.Write($"{items} ");
+        }
+
+        // Problem 03
+
 
         var people = new List<Persons>
         {
